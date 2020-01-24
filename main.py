a8a0cd9e6efdf9724c56b0d9ac1f4fa047b5242f
Tony Josi <Tony.Josi@analog.com>
2020-01-24 11:17:40 +0530

    prepends new o/p to begining of the file, edited wl

--- main.py
+++ main.py
@@ -5,22 +5,27 @@ import time
 import datetime
 import sys
 
+
 def get_price(link, op_id):
     page = requests.get(link)
     soup = BeautifulSoup(page.content, 'html5lib')
-    results = soup.find('div', attrs={'class':'_3Z5yZS NDB7oB _12iFZG _3PG6Wd'})
-    price = results.findAll('div', attrs = {'class':'_1vC4OE _3qQ9m1'})
+    results = soup.find(
+        'div', attrs={'class': '_3Z5yZS NDB7oB _12iFZG _3PG6Wd'})
+    price = results.findAll('div', attrs={'class': '_1vC4OE _3qQ9m1'})
+    # print(results)
     price = str(price)
     price_rate_lt = price.split("₹", 1)
-    price_rate_rt = price_rate_lt[1].split("<",1)
+    price_rate_rt = price_rate_lt[1].split("<", 1)
     op_price_list[op_id] = price_rate_rt[0]
     # print(price_rate_rt[0])
+
 
 def file_len(fname):
     with open(fname) as fp:
         for i, _ in enumerate(fp):
             pass
     return i + 1
+
 
 def op_to_file(dest):
     if (dest == 0):
@@ -29,31 +34,47 @@ def op_to_file(dest):
         op_mode = 'w'
     else:
         op_file_name = "wl_prices_master.txt"
-        op_mode = 'a'
-    
+        op_mode = 'r+'
+
+    old_data = ''
+
+    ''' check if file exists '''
+    try:
+        f = open(op_file_name)
+    except FileNotFoundError:
+        f = open(op_file_name, 'w+')
+    finally:
+        f.close()
+
     with open(op_file_name, op_mode) as f:
         if (dest != 0):
+            old_data = f.read()
+            f.seek(0)
             f.write("\n\n")
-            f.write("|------------------------------------------------------------------------------------|\n")
-            date_time = "|  / / / / / / / / / / /      " + str(datetime.datetime.now()) + "      / / / / / / / / / / /  |"
+            f.write(
+                "|------------------------------------------------------------------------------------|\n")
+            date_time = "|  / / / / / / / / / / /      " + \
+                str(datetime.datetime.now()) + "      / / / / / / / / / / /  |"
             f.write("%s\n" % date_time)
-            f.write("|------------------------------------------------------------------------------------|\n")
+            f.write(
+                "|------------------------------------------------------------------------------------|\n")
 
         for item in op_price_list:
             f.write("%s\n" % item)
 
         if (dest != 0):
-            f.write("|------------------------------------------------------------------------------------|\n")
-
+            f.write(
+                "|------------------------------------------------------------------------------------|\n")
+            f.write(old_data)
 
 
 if __name__ == '__main__':
 
     ip_filepath = "wish_list/wish_list.txt"
 
-    f_obj_r = open(ip_filepath, 'r') 
+    f_obj_r = open(ip_filepath, 'r')
     ip_count = file_len(ip_filepath)
-    #print(ip_count)
+    # print(ip_count)
 
     op_price_list = [0] * ip_count
     exit_flag = 0
@@ -86,7 +107,8 @@ if __name__ == '__main__':
             continue
         else:
             # print("work 1")
-            worker_1 = threading.Thread(target = get_price, args=(line1, ip_cntr,))
+            worker_1 = threading.Thread(
+                target=get_price, args=(line1, ip_cntr,))
             worker_1.start()
         ip_cntr += 1
         line2 = f_obj_r.readline()
@@ -96,7 +118,8 @@ if __name__ == '__main__':
             continue
         else:
             # print("work 1")
-            worker_2 = threading.Thread(target = get_price, args=(line2, ip_cntr,))
+            worker_2 = threading.Thread(
+                target=get_price, args=(line2, ip_cntr,))
             worker_2.start()
         ip_cntr += 1
         line3 = f_obj_r.readline()
@@ -105,7 +128,8 @@ if __name__ == '__main__':
             continue
         else:
             # print("work 1")
-            worker_3 = threading.Thread(target = get_price, args=(line3, ip_cntr,))
+            worker_3 = threading.Thread(
+                target=get_price, args=(line3, ip_cntr,))
             worker_3.start()
         ip_cntr += 1
         line4 = f_obj_r.readline()
@@ -114,7 +138,8 @@ if __name__ == '__main__':
             continue
         else:
             # print("work 1")
-            worker_4 = threading.Thread(target = get_price, args=(line4, ip_cntr,))
+            worker_4 = threading.Thread(
+                target=get_price, args=(line4, ip_cntr,))
             worker_4.start()
         ip_cntr += 1
 
@@ -126,7 +151,7 @@ if __name__ == '__main__':
             worker_4.join()
         except:
             pass
-    
+
     print("Exec. time:      {}".format(time.time() - start_time))
     if (len(sys.argv) > 1):
         if (sys.argv[1] == 's'):
